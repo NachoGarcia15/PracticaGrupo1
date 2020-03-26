499489c6e2d6be10589fb64eb8a9d4f35c6f4b22
pablovp <pvpcmg4@uma.es>
2020-03-26 11:26:48 +0100

    Añadido Llena y Vacia

--- Jarra.java
+++ Jarra.java
@@ -3,12 +3,19 @@ public class Jarra {//variables de instancia
     private int capacidad;
     public Jarra(int capacidadInicial){
         if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras “ +
+            throw new RuntimeException("ERROR: no se pueden crear jarras " +
             "con capacidad menor o igual que cero.");
         }
         this.capacidad=capacidadInicial;
     }
-    // Aquí vendrán después los métodos creados en la siguiente fase
+
+    public bool llena(){
+    	return this.cantidad=this.capacidad;
+    	}
+    	public bool vacia(){
+    	return this.cantidad=0;
+    	}
+
     public void metodoComunitario(){
     // Comentario incluido para pruebas
     System.out.println("Aqui escribimos todos");
