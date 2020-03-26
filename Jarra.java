e0dc0855b079916e4785c9291d4de2d8dd6c51b0
AlvaroHernandezUreña <0619701977@uma.es>
2020-03-26 13:05:29 +0100

    JarraV300

--- Jarra.java
+++ Jarra.java
@@ -1,19 +1,3 @@
- public class Jarra {//variables de instancia
-    private int cantidad;
-    private int capacidad;
-    public Jarra(int capacidadInicial){
-        if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero");
-        }
-        this.capacidad=capacidadInicial;
-    }
-    
-    // Aquí vendrán después los métodos creados en la siguiente fase
-    public void metodoComunitario(){
-    // Comentario incluido para pruebas
-    System.out.println("Aqui escribimos todos");
-    }
-
     public void llenaDesde(Jarra j){
     while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
     this.cantidad++;
@@ -24,12 +8,5 @@
     return ("("+this.capacidad+","+this.cantidad+")");
     }
 }
-    public int capacidad(){
-        return this.capacidad;
-    }
-
-    public int cantidad(){
-        return this.cantidad;
-    }
-}
+   
 }
