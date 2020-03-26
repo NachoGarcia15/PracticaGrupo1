bf2b0e6c1c0622deebbec7efde3b431d6c2e7ba4
AlvaroHernandezUreña <0619701977@uma.es>
2020-03-26 11:26:58 +0100

    llenadesdeTostring

--- Jarra.java
+++ Jarra.java
@@ -3,8 +3,7 @@ public class Jarra {//variables de instancia
     private int capacidad;
     public Jarra(int capacidadInicial){
         if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras “ +
-            "con capacidad menor o igual que cero.");
+            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero");
         }
         this.capacidad=capacidadInicial;
     }
@@ -12,5 +11,15 @@ public class Jarra {//variables de instancia
     public void metodoComunitario(){
     // Comentario incluido para pruebas
     System.out.println("Aqui escribimos todos");
+    }
+
+    public void llenaDesde(Jarra j){
+    while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+    this.cantidad++;
+    j.cantidad--;
+    }
+    }
+    public String toString(){
+    return ("("+this.capacidad+","+this.cantidad+")");
     }
 }
