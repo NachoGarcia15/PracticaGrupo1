256086f136c573a17460fc679b8ebb571aa24b99
AlvaroHernandezUreña <0619701977@uma.es>
2020-03-26 12:42:22 +0100

    JarraV2

--- Jarra.java
+++ Jarra.java
@@ -20,7 +20,7 @@
     while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
     this.cantidad++;
     j.cantidad--;
-    }
+        }
     }
     public String toString(){
     return ("("+this.capacidad+","+this.cantidad+")");
